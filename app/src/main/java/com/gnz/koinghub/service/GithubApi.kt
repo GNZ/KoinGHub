3ff5c91299b82a487f7fc9ee80562b3c0b12a335
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2018-10-08 01:12:34 +0200

    Create teh list adapter

--- app/src/main/java/com/gnz/koinghub/service/GithubApi.kt
+++ app/src/main/java/com/gnz/koinghub/service/GithubApi.kt
@@ -3,11 +3,15 @@ package com.gnz.koinghub.service
 import com.gnz.koinghub.data.RepoList
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
 interface GithubApi {
 
-    @GET("search/repositories?q=android+language:java+language:kotlin&sort=stars")
-    fun trendingAndroidRepos(@Query("per_page") perPage: Int, @Query("page") page: Int) : Single<RepoList>
+    @GET("search/repositories?q=android+language:java+language:kotlin+created:>{date}&sort=stars")
+    fun trendingAndroidRepos(@Path("date") date: String,
+                             @Query("per_page") perPage: Int,
+                             @Query("page") page: Int):
+            Single<RepoList>
 }
